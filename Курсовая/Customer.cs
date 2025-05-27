75dc3d7f70dbfc423fd33f583cc743dee02cfbbc
ClyZeR1 <sergeistarodyb@gmail.com>
2025-05-27 23:51:18 +0300

    Add files via upload

---
+++ Курсовая/Customer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Курсовая
+{
+    class Customer
+    {
+        public int Key { get; set; }
+        public string FIO { get; set; }
+        public int Number_pasport { get; set; }
+        public string Where { get; set; }
+        public int Number { get; set; }
+        public string Place { get; set; }
+        public int Kol_vo_days { get; set; }
+        public DateTime Date { get; set; }
+
+        public Customer(int key, string fio, int passport, string where, int number, string place, int kolvo, DateTime date)
+        {
+            Key = key;
+            FIO = fio;
+            Number_pasport = passport;
+            Where = where;
+            Number = number;
+            Place = place;
+            Kol_vo_days = kolvo;
+            Date = date;
+        }
+
+        public static Customer AddCustomer()
+        {
+            Console.Write("Введите ключ: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ФИО: ");
+            string fio = Console.ReadLine();
+            Console.Write("Введите номер паспорта: ");
+            int passport = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите город: ");
+            string city = Console.ReadLine();
+            Console.Write("Введите номер препарата: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите категорию: ");
+            string place = Console.ReadLine();
+            Console.Write("Введите кол-во упаковок: ");
+            int kolvo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите дату покупки (yyyy-MM-dd): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+
+            return new Customer(key, fio, passport, city, number, place, kolvo, date);
+        }
+
+        public static Customer FromFile(string line)
+        {
+            var parts = line.Split(';');
+            return new Customer(int.Parse(parts[0]), parts[1], int.Parse(parts[2]), parts[3], int.Parse(parts[4]), parts[5], int.Parse(parts[6]), DateTime.Parse(parts[7]));
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Ключ:{Key},ФИО:{FIO},Номер паспорта:{Number_pasport},Откуда:{Where}");
+            Console.WriteLine($"Номер:{Number},Место:{Place},Кол-во упаковок:{Kol_vo_days},Дата покупки:{Date:yyyy-MM-dd}");
+        }
+
+        public void Save()
+        {
+            using (StreamWriter writer = new StreamWriter("Покупатели.txt", true))
+            {
+                writer.WriteLine($"{Key};{FIO};{Number_pasport};{Where};{Number};{Place};{Kol_vo_days};{Date:yyyy-MM-dd}");
+            }
+        }
+
+        public static void City(List<Customer> customers)
+        {
+            Console.Write("Введите название города: ");
+            string cities = Console.ReadLine();
+            bool found = false;
+            foreach (var elem in customers)
+            {
+                if (elem.Where == cities)
+                {
+                    elem.Print();
+                    found = true;
+                }
+            }
+            if (!found)
+                Console.WriteLine("Нет клиентов из указанного города.");
+        }
+
+        public void Single()
+        {
+            if (Place == "Одноместный")
+                Print();
+        }
+    }
+}
---
+++ Курсовая/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Курсовая
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            General db = new General();
+            bool firstMenu = true;
+
+            while (firstMenu)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("ПЕРВОЕ МЕНЮ");
+                Console.ResetColor();
+                Console.WriteLine("[1] Считывание данных");
+                Console.WriteLine("[2] Добавление клиента");
+                Console.WriteLine("[3] Добавление служащего");
+                Console.WriteLine("[4] Добавление препарата");
+                Console.WriteLine("[5] Перейти в главное меню");
+
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        try
+                        {
+                            db.Customers = System.IO.File.ReadAllLines("Покупатели.txt").Select(Customer.FromFile).ToList();
+                            db.Employees = System.IO.File.ReadAllLines("Сотрудники.txt").Select(Employee.FromFile).ToList();
+                            Medicine.LoadMedicines(db.Medicines);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Считывание удалось");
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Считывание не удалось");
+                        }
+                        Console.ResetColor();
+                        Console.Write("Нажмите Enter для продолжения...");
+                        Console.ReadLine();
+                        break;
+                    case '2':
+                        var customer = Customer.AddCustomer();
+                        db.Customers.Add(customer);
+                        customer.Save();
+                        db.Customers = System.IO.File.ReadAllLines("Покупатели.txt").Select(Customer.FromFile).ToList();
+                        Console.WriteLine("Новый клиент успешно добавлен");
+                        Console.Write("Нажмите Enter для продолжения...");
+                        Console.ReadLine();
+                        break;
+                    case '3':
+                        var employee = Employee.AddEmployee();
+                        db.Employees.Add(employee);
+                        employee.Save();
+                        Console.WriteLine("Новый служащий успешно добавлен");
+                        Console.Write("Нажмите Enter для продолжения...");
+                        Console.ReadLine();
+                        break;
+                    case '4':
+                        var med = Medicine.AddMedicine();
+                        db.Medicines.Add(med);
+                        med.Save();
+                        db.Medicines = System.IO.File.ReadAllLines("Препараты.txt").Select(Medicine.FromFile).ToList();
+                        Console.WriteLine("Препарат успешно добавлен");
+                        Console.Write("Нажмите Enter для продолжения...");
+                        Console.ReadLine();
+                        break;
+                    case '5':
+                        firstMenu = false;
+                        break;
+                }
+            }
+
+            bool mainMenu = true;
+            while (mainMenu)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("ГЛАВНОЕ МЕНЮ");
+                Console.ResetColor();
+                Console.WriteLine("[1] Покупатели");
+                Console.WriteLine("[2] Сотрудники");
+                Console.WriteLine("[3] Препараты");
+                Console.WriteLine("[4] Выход из программы");
+
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        Console.Clear();
+                        foreach (var c in db.Customers) c.Print();
+                        Console.Write("Нажмите Enter для возврата...");
+                        Console.ReadLine();
+                        break;
+                    case '2':
+                        Console.Clear();
+                        foreach (var e in db.Employees) e.Print();
+                        Console.Write("Нажмите Enter для возврата...");
+                        Console.ReadLine();
+                        break;
+                    case '3':
+                        Console.Clear();
+                        Medicine.ShowAll(db.Medicines);
+                        break;
+                    case '4':
+                        mainMenu = false;
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
